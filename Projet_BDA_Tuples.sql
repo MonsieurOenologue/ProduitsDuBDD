43ed62f55fbcbf0eae5972e923586be98a37f26e
MonsieurOenologue <thomas.lfbvr@gmail.com>
2016-11-05 14:06:00 +0100

    Little fix

--- Projet_BDA_Tuples.sql
+++ Projet_BDA_Tuples.sql
@@ -6,7 +6,7 @@ INSERT INTO Repas VALUES
     Fromage_t('Comté', 'Vache', 5.21),
     Client_t('Jean', 0666953278, Adresse_t('5 rue Albert Premier', 34000, 'Montpellier')),
     Vins_t('Château Vieux Robin, Cru Bourgeois', 10, 1999, 11),
-    Charcuterie_t(100, 'France - Alsace', 3, "Saucisse", 5)
+    Charcuterie_t(100, 'France - Alsace', 3, 'Saucisse', 5)
 );
 /
 INSERT INTO Client VALUES(Client_t('Chloé', 0656333609, Adresse_t('22 rue de la Vaslière', 34000, 'Montpellier')));
@@ -17,7 +17,7 @@ INSERT INTO Repas VALUES
     Fromage_t('Mimolette', 'Vache', 5.92),
     Client_t('Chloé', 0656333609, Adresse_t('22 rue de la Vaslière', 34000, 'Montpellier')),
     Vins_t('Château Vieux Robin, Cru Bourgeois', 9, 1999, 11),
-    Charcuterie_t(75, 'France - Bretagne', 3, "Saucisse", 5)
+    Charcuterie_t(75, 'France - Bretagne', 3,'Saucisse', 5)
 );
 /
 INSERT INTO Client VALUES(Client_t('Robert', 0668321234, Adresse_t('3 Avenue de Rome', 34000, 'Montpellier')));
@@ -28,7 +28,7 @@ INSERT INTO Repas VALUES
     Fromage_t('Tome de Savoie', 'Vache', 7.32),
     Client_t('Robert', 0668321234, Adresse_t('3 Avenue de Rome', 34000, 'Montpellier')),
     Vins_t('Château Le Font du Broc, Maestro', 12, 2010, 29),
-    Charcuterie_t(100, 'Espagne - Catalogne', 3, "Saucisse", 5)
+    Charcuterie_t(100, 'Espagne - Catalogne', 3, 'Saucisse', 5)
 );
 /
 INSERT INTO Client VALUES(Client_t('Ernesto', 0626884251, Adresse_t('18 Avenue d Occitanie', 34000, 'Montpellier')));
@@ -39,6 +39,6 @@ INSERT INTO Repas VALUES
     Fromage_t('Fromage de chèvre', 'Chèvre', 4.43),
     Client_t('Ernesto', 0626884251, Adresse_t('18 Avenue d Occitanie', 34000, 'Montpellier')),
     Vins_t('Château Vieux Robin, Cru Bourgeois', 11, 1999, 11),
-    Charcuterie_t(100, 'Italie - Toscane', 3, "Saucisse", 5)
+    Charcuterie_t(100, 'Italie - Toscane', 3, 'Saucisse', 5)
 );
 /
