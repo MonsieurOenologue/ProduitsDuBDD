1375efdafd78e521594d69e8de726d28b351888d
MonsieurOenologue <thomas.lfbvr@gmail.com>
2016-11-09 20:11:48 +0100

    changes

--- Projet_BDA_Tuples.sql
+++ Projet_BDA_Tuples.sql
@@ -3,7 +3,7 @@ INSERT INTO Repas VALUES
 (
     25,
     Pain_t('Baguette', 'Four', 2, 1.42, 2),
-    List_of_fromages_t(List_of_fromages(Fromage_t('bite', 'bite', 1, 1), Fromage_t('bite', 'bite', 1, 1))),
+    List_of_fromages_t(Fromage_t('bite', 'bite', 1, 1), Fromage_t('bite', 'bite', 1, 1)),
     Client_t('Jean', 0666953278, Adresse_t('5 rue Albert Premier', 34000, 'Montpellier')),
     Vin_t('Château Vieux Robin, Cru Bourgeois', 10, 'rouge', 1999, 11, 2),
     Charcuterie_t('Saucisse de canard','France - Alsace', 100, 3, 5)
